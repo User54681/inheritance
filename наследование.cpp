eb5af32b2e114e3220aff5161751b059fa20b2f7
User54681 <anzolotylh@gmail.com>
2023-12-10 20:55:27 +0300

    Добавьте файлы проекта.

---
+++ наследование.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+
+using std::string;
+
+class University {
+private:
+    string name;
+    string country;
+    int ranks;
+    std::vector<Student*> students;
+public:
+    University()
+    {
+
+    }
+    University(string name, string country)
+    {
+        this->name = name;
+        this->country = country;
+    }
+    void AddStudent(Student& student) {
+        students.push_back(&student);
+    }
+};
+
+class Student {
+private:
+    string name;
+    int course;
+    int age;
+public:
+    Student() {
+
+    }
+    Student(string name, int course) {
+        this->name = name;
+        this->course = course;
+    }
+};
+
+class Employee {
+private:
+    string name;
+    int age;
+    string post;
+public:
+    Employee() {
+
+    }
+    Employee(string name) {
+        this->name = name;
+    }
+};
+
+class bachelor : Student {
+private:
+    string speciality;
+public:
+
+};
+
+class master : Student {
+private:    
+    string speciality;
+public:
+
+};
+
+int main() {
+    University myUniversity("My University", "USA");
+    Student myStudent("John Doe", 1);
+    myUniversity.AddStudent(myStudent);
+    myUniversity.PrintStudents();
+}
