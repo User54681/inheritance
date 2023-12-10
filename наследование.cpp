cf455ed17783a8cf4037a9b4de5801647f5c8bca
User54681 <anzolotylh@gmail.com>
2023-12-10 21:49:34 +0300

    конец?

--- наследование.cpp
+++ наследование.cpp
@@ -3,27 +3,6 @@
 
 using std::string;
 
-class University {
-private:
-    string name;
-    string country;
-    int ranks;
-    std::vector<Student*> students;
-public:
-    University()
-    {
-
-    }
-    University(string name, string country)
-    {
-        this->name = name;
-        this->country = country;
-    }
-    void AddStudent(Student& student) {
-        students.push_back(&student);
-    }
-};
-
 class Student {
 private:
     string name;
@@ -36,6 +15,12 @@ public:
     Student(string name, int course) {
         this->name = name;
         this->course = course;
+    }
+    string GetName() {
+        return name;
+    }
+    int GetCourse() {
+        return course;
     }
 };
 
@@ -56,6 +41,8 @@ public:
 class bachelor : Student {
 private:
     string speciality;
+    int quality_of_students;
+    string department;
 public:
 
 };
@@ -63,13 +50,44 @@ public:
 class master : Student {
 private:    
     string speciality;
+    int quality_of_students;
+    string department;
 public:
 
 };
 
+class University {
+private:
+    string name;
+    string country;
+    int ranks;
+    std::vector<Student*> students;
+public:
+    University()
+    {
+
+    }
+    University(string name, string country)
+    {
+        this->name = name;
+        this->country = country;
+    }
+    void AddStudent(Student& student) {
+        students.push_back(&student);
+    }
+    void PrintStudents() {
+        for (auto& student : students) std::cout << student->GetName() << " " << student->GetCourse() << "\n";
+    }
+    void PrintUniversity() {
+        std::cout << name << " " << country << "\n";
+    }
+};
+
 int main() {
-    University myUniversity("My University", "USA");
-    Student myStudent("John Doe", 1);
+    University myUniversity("RUDN", "Russia");
+    University AnotherUniversity("MSU", "Russia");
+    Student myStudent("Anna Zolotykh", 2);
     myUniversity.AddStudent(myStudent);
     myUniversity.PrintStudents();
+    AnotherUniversity.PrintUniversity();
 }
